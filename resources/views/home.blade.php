aefa18a6e32e42b9535746e11afe38bb3b0bd957
Maxime Jacquet <maximejacquet@Maximes-MacBook-Pro-2.local>
2017-11-18 11:20:31 +0100

    menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,13 @@
 --}}
 @extends('layouts.app')
 @section('content')
-  {{-- 
 
-    -- block1
 
-  --}}
   <section class="block">
     <div class="container">
     <h1>Spoutnik</h1>
     </div>
   </section>
+
+
 @endsection
