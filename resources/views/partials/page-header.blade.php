c284f8ba39c9c27cb92d2123f54a0d5c80410327
W0Z4 <julienvanroy@yahoo.fr>
2017-11-20 21:33:34 +0100

    Probleme

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -2,7 +2,7 @@
   $urlImage = get_post_thumbnail_id();
   $urlImage = wp_get_attachment_image_src($urlImage,'large');
 ?>
-<div class="page-header" style = 'background-image:url(<?= $urlImage[0] ?>);'>
+<div class="page-header" style='background-image:url(<?= $urlImage[0] ?>);'>
   <h1><?php the_field('bandeau_amorce'); ?></h1>
   <h2><?php the_field('bandeau_accroche'); ?></h2>
 </div>
