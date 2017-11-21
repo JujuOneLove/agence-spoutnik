f50d53050ac09a85034f854c17229e6126416fb0
W0Z4 <julienvanroy@yahoo.fr>
2017-11-21 21:27:29 +0100

    Baniere Terminé

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -3,6 +3,8 @@
   $urlImage = wp_get_attachment_image_src($urlImage,'large');
 ?>
 <div class="page-header" style='background-image:url(<?= $urlImage[0] ?>);'>
-  <h1><?php the_field('bandeau_amorce'); ?></h1>
-  <h2><?php the_field('bandeau_accroche'); ?></h2>
+  <div class="titlePage">
+    <h3 id="amorce"><?php the_field('bandeau_amorce'); ?></h3>
+    <h3><span><?php the_field('bandeau_accroche'); ?></span></h3>
+  </div>
 </div>
