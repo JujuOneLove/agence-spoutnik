0e1af2d52db829b082c0dcfc254a2bbe94fd49db
Florian Nalenne <florian.nalenne@gmail.com>
2017-11-23 21:39:54 +0100

    test mise en ligne references

--- resources/views/references.blade.php
+++ resources/views/references.blade.php
@@ -3,11 +3,37 @@
 --}}
 
 @extends('layouts.app')
+@include('partials.page-header')
+
+
 
 @section('content')
-<section class="block">
-  <div class="container">
-    <?php the_title();?>
+<section class>  
+  <div class="reference_container">
+    <div class="filter_works"> 
+        <a href="#">Print</a>
+        <p>/</p>
+        <a href="#">Web</a>
+        <p>/</p>
+        <a href="#">Réferencement</a>
+    </div>
+    <div class="references_works">
+    <?php    
+        $query = new WP_Query( array( 'post_type' => 'references') );
+ 
+            if ( $query->have_posts() ) : ?>
+                <?php while ( $query->have_posts() ) : $query->the_post(); ?>	
+                    <div class="entry">
+                        <h2 class="title"><?php the_title(); ?></h2>
+
+                    </div>
+                <?php endwhile; wp_reset_postdata(); ?>
+                <!-- show pagination here -->
+            <?php else : ?>
+                <!-- show 404 error here -->
+            <?php endif; ?>
+       
+    </div>
   </div>
 </section>
 @endsection
