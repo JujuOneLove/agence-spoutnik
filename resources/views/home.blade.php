dff39d7c5547318823e75077e8cb7000ca8f9e31
Maxime Jacquet <maximejacquet@Maximes-MacBook-Pro-2.local>
2017-11-23 22:41:49 +0100

    fix home.scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,25 @@
 --}}
 @extends('layouts.app')
 
-
-
 <?php
-$urlImage = get_post_thumbnail_id(); 
-$urlImage = wp_get_attachment_image_src($urlImage,'large'); 
+  $urlImage = get_post_thumbnail_id(); 
+  $urlImage = wp_get_attachment_image_src($urlImage,'large'); 
 ?>
 
 
-<section class="backgroundhome" style='background-image:url(<?= $urlImage[0] ?>);'>
- @section('content')
-    <div class="container_page">
+@section('content')
+  <section class="backgroundhome" style='background-image:url(<?= $urlImage[0] ?>);'>
+      <div class="container_page">
         <div class="container_texte"> 
           <div class="texte_accueil">
             <p><?php echo get_field('accueil_amorce'); ?></p> 
             <h1>
                 <span><?php echo get_field('accueil_accroche'); ?></span>
             </h1>
-            <p><?php echo get_field('accueil_texte'); ?></p>
+            <h2><?php echo get_field('accueil_texte'); ?></h2>
           </div>
         </div>
-        <a href="<?php the_field('agence_button'); ?> " class="explore_button">Explorer</a>
-    </div>
-</section>
+        <a href="<?php the_field('agence_button'); ?>" class="explore_button">Explorer</a>
+      </div>
+  </section>
 @endsection
