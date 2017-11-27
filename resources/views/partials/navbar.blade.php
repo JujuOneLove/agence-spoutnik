1d67a1f54972b23d2c0b74380e47b24ea326f6b3
Vincent Battez <vincent.battez.pro@gmail.com>
2017-11-27 21:24:13 +0100

    navbar dropdown

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,21 +5,45 @@
   //$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
   $menuitems = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'DESC' ) );
 ?>
-  <nav class="{{$menu_name}}">
-    {{--  <div class="primary-nav__logo">
-      <a href="{{ get_theme_file_uri() }}/dist/images/logo.svg">
-        LOGO
-      </a>
-    </div>  --}}
-    <ul>
-      @foreach( $menuitems as $item )
 
+<nav class="{{$menu_name}}">
+  <ul class="dropdown menu" data-dropdown-menu>
+    @php ($count = 0)
+    @php ($submenu = false)
+
+      @foreach($menuitems as $item)
+          @php ($link = $item->url)
+          @php ($title = $item->title)
+          @if ( !$item->menu_item_parent )
+            @php ($parent_id = $item->ID)
       <li class="item">
-        <a href="{{$item->url}}" class="title">
-          {{$item->title}}
-        </a>
-      </li>
+          <a href="{{$item->url}}" class="title">
+              {{$item->title}}
+          </a>
+      <?php endif; ?>
 
-      @endforeach
-    </ul>
-  </nav>
+          <?php if ( $parent_id == $item->menu_item_parent ): ?>
+
+              <?php if ( !$submenu ): $submenu = true; ?>
+              <ul class="menu">
+              <?php endif; ?>
+
+                  <li class="item">
+                      <a href="<?php echo $link; ?>" class="title"><?php echo $title; ?></a>
+                  </li>
+
+              <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
+              </ul>
+              <?php $submenu = false; endif; ?>
+
+          <?php endif; ?>
+
+      <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id ): ?>
+      </li>                           
+      <?php $submenu = false; endif; ?>
+
+  @php($count++) 
+  @endforeach
+
+  </ul>
+</nav>
