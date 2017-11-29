1c94d25d47d70796d691915787647fde1d6d4d5b
Florian Nalenne <florian.nalenne@gmail.com>
2017-11-29 21:11:50 +0100

    references

--- resources/views/references.blade.php
+++ resources/views/references.blade.php
@@ -5,10 +5,7 @@
 @extends('layouts.app')
 @include('partials.page-header')
 
-<?php
-  $entry_img = get_post_thumbnail_id(); 
-  $entry_img = wp_get_attachment_image_src($entry_img,'large'); 
-?>
+
 
 
 
@@ -28,7 +25,11 @@
  
             if ( $query->have_posts() ) : ?>
                 <?php while ( $query->have_posts() ) : $query->the_post(); ?>	
-                    <div class="entry" style='background-image:url(<?= $entry_img[0] ?>);'>
+                <?php
+                    $entryimg = get_post_thumbnail_id(); 
+                    $entryimg = wp_get_attachment_image_src($entryimg,'large'); 
+                ?>
+                    <div class="entry" style='background-image:url(<?= $entryimg[0] ?>); background-position:center; background-size:cover;'>
                             <div class="container_symbol info_entry">   
                                 <div class= "symbol">+</div> 
                             </div>  
