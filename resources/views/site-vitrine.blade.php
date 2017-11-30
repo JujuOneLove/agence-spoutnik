b312f3fff84d37ca5b4363076d5e7cb505eb310e
Benoit <benoit.carquillat@gmail.com>
2017-11-30 21:06:40 +0100

    merged

--- resources/views/site-vitrine.blade.php
+++ resources/views/site-vitrine.blade.php
@@ -7,6 +7,11 @@
 @section('content')
 @include('partials.page-header')
 
-<h1>Test Site vitrine </h1>
+<div class="row align-center">
+  <div class='small-12 column'>
+    <h2><?php the_field('titre_site_vitrine'); ?> </h2>
+    <p><?php the_field('sous_titre_site_vitrine'); ?></p>
+  </div>
+</div>
 
 @endsection
