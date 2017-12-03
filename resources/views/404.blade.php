e42b7a85afb16fc0e499d965a8976c98cd8e7123
W0Z4 <julienvanroy@yahoo.fr>
2017-12-03 18:58:25 +0100

    Ajout page 404 (Bug avec la nav pour la page 404)

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,30 @@
+{{--
+  Template Name: 404
+--}}
 @extends('layouts.app')
 
-@section('content')
-  @include('partials.page-header')
+<?php
+  $urlImage = get_post_thumbnail_id();
+  $urlImage = wp_get_attachment_image_src($urlImage,'large');
+?>
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+@section('content')
+  <section class="backgroundhome" style='background-image:url(<?= $urlImage[0] ?>);'>
+      <div class="container_page">
+        <div class="container_texte">
+          <div class="texte_accueil">
+            <p><?= get_field('accueil_amorce'); ?></p>
+            <h1>
+                <span><?= get_field('accueil_accroche'); ?></span>
+            </h1>
+            <p><?= get_field('accueil_texte'); ?></p>
+          </div>
+        </div>
+
+        <h2>
+            <a href="<?php the_field('accueil_bouton'); ?>" class="explore_button"><?php the_field('accueil_button_texte');?></a>
+        </h2>
+
+      </div>
+  </section>
 @endsection
