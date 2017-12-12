daeb2fbe26b0596832b09dc40dddf79f8253a639
mehechi <maxime1jacquet@gmail.com>
2017-12-12 23:18:53 +0100

    add reference

--- resources/views/references.blade.php
+++ resources/views/references.blade.php
@@ -39,10 +39,9 @@
             foreach ( $terms as $term ){$x .= ' tag-'.$term;}
 
 
-            $o = '<li class="entry large-3 medium-6 small-12 columns '.$x.'" style="background-image:url('.$entryimg[0].'); background-position:center; background-size:cover;">
-            <div class="container_symbol info_entry"><div class="symbol">+</div> </div>
-            
-            <div class="container_title info_entry"><h3 class="title"><a href="'.get_permalink().'">'.get_the_title().'</a></h3></div></li>';
+            $o = '<li class="entry large-3 medium-6 small-12 columns '.$x.'" style="background-image:url('.$entryimg[0].');">
+            <div class="container_title"><h3><a href="'.get_permalink().'">'.get_the_title().'</a></h3></div>
+            </li>';
 
             if(isset($_GET['real'])){
                 if(stristr($x, $_GET['real']) === FALSE){} else{
@@ -51,7 +50,6 @@
             } else{
                 echo $o;
             } ?>
-
 
             
         <?php endwhile; wp_reset_postdata(); ?>
---
+++ resources/views/single-references.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    <section class='contenu-prestation references'>
+      <?php $i=0;if( have_rows('repeater_single_prestation') ): while ( have_rows('repeater_single_prestation') ) : the_row();?>
+      <?php $img = get_sub_field('picto');?>
+
+        <div class="row aling-middle <?php if($i%2!=0){ echo 'reverse';} ?>">
+
+          <div class='small-12 medium-5 large-6 contenu-prestation__image flex-container align-middle align-center'>
+            <img class='style-svg' src="<?=$img['url']?>"/>
+          </div>
+
+          <div class='small-12 medium-7 large-6 contenu-prestation__texte'>
+            <?php if(get_sub_field('titre_h2')){?><h2><?= get_sub_field('titre_h2');?></h2><?php }?><?= get_sub_field('texte');?>
+          </div>
+
+        </div>
+      <?php $i++; endwhile; endif;?>
+    </section>
+  @endwhile
+@endsection
